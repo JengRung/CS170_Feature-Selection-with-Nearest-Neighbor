c612215a45a44ac997d38714c692941f7c0cc96b
Justin <jengrung2255@gmail.com>
2022-11-28 01:31:16 -0800

    Finish forwardSelection and backwardElimination

--- program.py
+++ program.py
@@ -1,8 +1,5 @@
 import pandas as pd
-
-# Using pandas to convert txt to csv
-inputFile = 'datasets/CS170_Small_Data__96.txt' 
-dataFrame = pd.read_csv(inputFile, header = None, delim_whitespace=True,)
+import time
 
 def nearestNeighbor(df, indexList):
     '''
@@ -30,9 +27,93 @@ def nearestNeighbor(df, indexList):
         # Check if the type of the point is the same as the type of the nearest neighbor
         if df[0][i] == df[0][minDist[1]]:
             correctCount += 1
-        # # Print the index of the point and the distance
-        # print(i, minDist[1], minDist[0])
 
-    print("Accuracy: ", correctCount/size)
+    accuracy = correctCount / size
+    print("Using feature(s) ", indexList, " accuracy is ", float("{:.2f}".format(accuracy*100)), "%")
+    return accuracy
 
-nearestNeighbor(dataFrame, [1, 3, 6])
+def forwardSelection(df):
+    # Current array append the best feature (1) from each iteration, increase until the size match the number of attributes
+    curArr = []
+    # Best array store the best feature set (1+) from each iteration, return the best feature set at the end
+    bestArr = []
+    # Number of attributes
+    rowSize = df.shape[1]
+    while(len(curArr) < rowSize - 1):
+        result = []
+        for i in range(1, rowSize, 1):
+            # Call the nearestNeighbor function
+            if i not in curArr:
+                tempArr = []
+                tempArr.extend(curArr)
+                tempArr.append(i)
+                result.append([nearestNeighbor(df, tempArr), i])
+        best = max(result)
+        
+        bestSet = []
+        bestSet.extend(curArr)
+        bestSet.extend([best[1]])
+        bestArr.append([best[0] ,bestSet])
+        
+        curArr.append(best[1])
+        print("Feature set ", bestSet, " was best, accuracy is ", float("{:.2f}".format(best[0]*100)), "%\n")
+
+    bestSet = max(bestArr)
+    print("Finish search!! The best feature subset is ", bestSet[1], ", which has an accuracy of ", float("{:.2f}".format(bestSet[0]*100)), "%")
+    
+
+def backwardElimination(df):
+    # Current array append the best feature (1) from each iteration, decrease until the size match 0
+    curArr = []
+    # Best array store the best feature set (1+) from each iteration, return the best feature set at the end
+    bestArr = []
+    # Number of attributes
+    rowSize = df.shape[1]
+    for i in range(1, rowSize, 1):
+        curArr.append(i)
+    
+    # Test all the subsets
+    allSetAccuracy = nearestNeighbor(df, curArr)
+    bestArr.append([allSetAccuracy, curArr])
+    print("Feature set ", curArr, " was best, accuracy is ", float("{:.2f}".format(allSetAccuracy*100)), "%\n")
+    
+    # Start to eliminate the feature until the size reach 0
+    while(len(curArr) != 0):
+        result = []
+        for i in range(1, rowSize, 1):
+            # Call the nearestNeighbor function
+            if i in curArr:
+                tempArr = []
+                tempArr.extend(curArr)
+                tempArr.remove(i)
+                result.append([nearestNeighbor(df, tempArr), i])
+        best = max(result)
+        
+        bestSet = []
+        bestSet.extend(curArr)
+        bestSet.remove(best[1])
+        bestArr.append([best[0] ,bestSet])
+        
+        curArr.remove(best[1])
+        print("Feature set ", bestSet, " was best, accuracy is ", float("{:.2f}".format(best[0]*100)), "%\n")
+
+    bestSet = max(bestArr)
+    print("Finish search!! The best feature subset is ", bestSet[1], ", which has an accuracy of ", float("{:.2f}".format(bestSet[0]*100)), "%")
+
+
+def main():
+    start = time.time()
+    
+    # inputFile = 'datasets/CS170_Small_Data__6.txt' 
+    inputFile = 'datasets/CS170_Large_Data__96.txt' 
+    # Using pandas to convert txt to csv
+    dataFrame = pd.read_csv(inputFile, header = None, delim_whitespace=True,)
+    
+    forwardSelection(dataFrame)
+    # backwardElimination(dataFrame)
+    
+    end = time.time()
+    print("Time taken is ", float("{:.2f}".format(end - start)), " seconds")
+
+if __name__ == "__main__":
+    main()
